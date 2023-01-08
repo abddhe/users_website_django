b151ed59bc5a55716f82ba3b9b840c6256704322
Abdallah Dheir <tareqm577@gmail.com>
2023-01-08 16:41:18 +0000

    First commit && create users app

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import include, path
+from users.views import dashboard
+urlpatterns = [
+    path("dashboard/", dashboard, name="dashboard"),
+    path("accounts/", include("django.contrib.auth.urls")),
+]
